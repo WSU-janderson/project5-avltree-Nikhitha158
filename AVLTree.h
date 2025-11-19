ce3ecaf9beaf5ee9441817f04e135b3547a1a7c0
Nikhitha158 <palakurla.2@wright.edu>
2025-11-18 22:22:21 -0500

    update

--- AVLTree.h
+++ AVLTree.h
@@ -15,15 +15,15 @@ using namespace std;
 
 class AVLTree {
 public:
-    using KeyType = std::string;
+    using KeyType   = std::string;
     using ValueType = size_t;
 
 protected:
     class AVLNode {
     public:
-        KeyType key;
+        KeyType   key;
         ValueType value;
-        size_t height;
+        size_t    height;
 
         AVLNode* left;
         AVLNode* right;
@@ -34,8 +34,8 @@ protected:
         // 0, 1 or 2
         size_t numChildren() const {
             size_t count = 0;
-            if (left) count++;
-            if (right) count++;
+            if (left)  ++count;
+            if (right) ++count;
             return count;
         }
 
@@ -44,40 +44,49 @@ protected:
             return !left && !right;
         }
 
-        // number of hops to deepest leaf node
+        // stored height of this node
         size_t getHeight() const {
             return height;
         }
 
         // Update height based on children's heights
         void updateHeight() {
-            size_t leftHeight = left ? left->height : 0;
+            size_t leftHeight  = left  ? left->height  : 0;
             size_t rightHeight = right ? right->height : 0;
             height = 1 + std::max(leftHeight, rightHeight);
         }
 
-        // Get balance factor
+        // Get balance factor (left height - right height)
         int getBalance() const {
-            size_t leftHeight = left ? left->height : 0;
+            size_t leftHeight  = left  ? left->height  : 0;
             size_t rightHeight = right ? right->height : 0;
             return static_cast<int>(leftHeight) - static_cast<int>(rightHeight);
         }
     };
 
     AVLNode* root;
-    size_t treeSize;
+    size_t   treeSize;
 
     // Recursive helper methods
-    bool insert(AVLNode*& node, const KeyType& key, ValueType value);
-    bool remove(AVLNode*& node, const KeyType& key);
-    bool contains(const AVLNode* node, const KeyType& key) const; // Fixed: made this the helper
+    bool    insert(AVLNode*& node, const KeyType& key, ValueType value);
+    bool    remove(AVLNode*& node, const KeyType& key);
+    bool    contains(const AVLNode* node, const KeyType& key) const;
     AVLNode* getNode(AVLNode* node, const KeyType& key) const;
-    void findRange(const AVLNode* node, const KeyType& lowKey, const KeyType& highKey,
-                   vector<KeyType>& result) const;
+
+    // NOTE: findRange now returns VALUES (ValueType) per spec
+    void findRange(const AVLNode* node,
+                   const KeyType& lowKey,
+                   const KeyType& highKey,
+                   vector<ValueType>& result) const;
+
     void getKeys(const AVLNode* node, vector<KeyType>& result) const;
+
     AVLNode* copyTree(const AVLNode* otherNode);
-    void clearTree(AVLNode* node);
-    void printTree(ostream& os, const AVLNode* node, size_t depth = 0) const;
+    void     clearTree(AVLNode* node);
+
+    void printTree(ostream& os,
+                   const AVLNode* node,
+                   size_t depth = 0) const;
 
     // Rotation methods
     void rotateLeft(AVLNode*& node);
@@ -85,8 +94,8 @@ protected:
     void balanceNode(AVLNode*& node);
 
     // Remove helper
-    bool removeNode(AVLNode*& node);
-    AVLNode* findMin(AVLNode* node);
+    bool     removeNode(AVLNode*& node);
+    AVLNode* findMin(AVLNode* node) const;
 
 public:
     AVLTree();
@@ -97,11 +106,20 @@ public:
     // Public interface
     bool insert(const KeyType& key, ValueType value);
     bool remove(const KeyType& key);
+
+    // Normal semantics: true if key is in the tree, false otherwise
     bool contains(const KeyType& key) const;
+
     std::optional<ValueType> get(const KeyType& key) const;
+
     ValueType& operator[](const KeyType& key);
-    vector<KeyType> findRange(const KeyType& lowKey, const KeyType& highKey) const;
+
+    // Returns VALUES in the key range [lowKey, highKey]
+    vector<ValueType> findRange(const KeyType& lowKey,
+                                const KeyType& highKey) const;
+
     vector<KeyType> keys() const;
+
     size_t size() const;
     size_t getHeight() const;
 
